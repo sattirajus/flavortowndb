95d6e1c2650a96efc168a1ae2fd2819cf09f823e
sattirajus <srikanthsattiraju@gmail.com>
2026-04-28 17:00:03 +0000

    Make if-statement naming consistent

--- main.c
+++ main.c
@@ -8,20 +8,20 @@
 int main(int argc, char *argv[]) {
     open_log_file();
 
-    if(argc == 0) {
+    if (argc == 0) {
         panic("argc is 0.");
     }
 
-    if(argc == 1) {
+    if (argc == 1) {
         show_help_message(argv[0]);
-    } else if(argc == 2) {
-        if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+    } else if  (argc == 2) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
             show_help_message(argv[0]);
         } else {
             panic("Incorrect usage of command line arguments.");
         }
-    } else if(argc == 3) {
-        if(strcmp(argv[1], "runscript") == 0) {
+    } else if (argc == 3) {
+        if (strcmp(argv[1], "runscript") == 0) {
             runscript(argv[2]);
         } else {
             panic("Incorrect usage of command line arguments.");
